6d118ae8ca808da00dd79d6d74e7805114d43e0b
At0micTurtle <carltrep@videotron.ca>
2023-08-10 21:20:39 -0400

    Updates

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -2,10 +2,12 @@ import mongoose from 'mongoose';
 
 const connectDB = (url) => {
   mongoose.set('strictQuery', true);
-
   mongoose.connect(url)
     .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.error('Failed to connect to MongoDB');
+      console.error(err);
+    });
 };
 
 export default connectDB;
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -20,7 +20,7 @@ router.route('/').get(async (req, res) => {
     const posts = await Post.find({});
     res.status(200).json({ success: true, data: posts });
   } catch (error) {
-    res.status(500).json({ success: false, message: error });    
+    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });    
   }
 });
 
@@ -38,7 +38,7 @@ router.route('/').post(async (req, res) => {
 
     res.status(201).json({ success: true, data: newPost});
   } catch (error) {
-    res.status(500).json({ success: false, message: error });
+    res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
   }
 });
 
