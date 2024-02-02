3883c7430b6ae93111c23cbd1f0e0533bddf3356
At0micTurtle <carltrep@videotron.ca>
2024-02-02 10:18:46 -0500

    Add express-rate-limit middleware to limit API requests

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -9,37 +9,50 @@ dotenv.config();
 const router = express.Router();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // limit each IP to 100 requests per windowMs
 });
 
 // Get all posts
-router.route('/').get(async (req, res) => {
-  try {
-    const posts = await Post.find({});
-    res.status(200).json({ success: true, data: posts });
-  } catch (error) {
-    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });    
-  }
+router.route('/').get(limiter, async (req, res) => {
+	try {
+		const posts = await Post.find({});
+		res.status(200).json({ success: true, data: posts });
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: 'Fetching posts failed, please try again',
+		});
+	}
 });
 
 // Create a post
 router.route('/').post(async (req, res) => {
-  try {
-    const { name, prompt, photo } = req.body;
-    const photoUrl = await cloudinary.uploader.upload(photo);
+	try {
+		const { name, prompt, photo } = req.body;
+		const photoUrl = await cloudinary.uploader.upload(photo);
 
-    const newPost = new Post.create({
-      name,
-      prompt,
-      photo: photoUrl.url,
-    });
+		const newPost = new Post.create({
+			name,
+			prompt,
+			photo: photoUrl.url,
+		});
 
-    res.status(201).json({ success: true, data: newPost});
-  } catch (error) {
-    res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
-  }
+		res.status(201).json({ success: true, data: newPost });
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: 'Unable to create a post, please try again',
+		});
+	}
 });
 
 export default router;
